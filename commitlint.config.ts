1fe8c87b9d64a11a711b6df99cd8d3bedfe5c733
William Giammona <wgiammona@gmail.com>
2024-12-11 19:01:32 +0200

    refactor: updated file to ts

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,4 +1,6 @@
-module.exports = {
+import type { UserConfig } from '@commitlint/types';
+
+const Configuration: UserConfig = {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'type-enum': [
@@ -25,3 +27,5 @@ module.exports = {
     'scope-case': [2, 'always', 'lower-case'],
   },
 };
+
+export default Configuration;
